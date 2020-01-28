b5de5790ce652618592bcf6aabaa1b36cc80cc67
Joey Wong <wong1@ualberta.ca>
2020-01-27 17:26:00 -0800

    added error to test

--- test_ci.py
+++ test_ci.py
@@ -10,7 +10,7 @@ def test_helloworld():
     r = requests.get("http://localhost:8085/hello-world").text
     data = json.loads(r)
     print(data['content']+"wtf")
-    assert(data['content']=="Hello, Stranger!")
+    assert(data['content']=="Fail, Stranger!")
 # 1. Add two more tests here of your choice. I will explain the API.
 #    Make sure to verify the necessary info, e.g., status code, response data.
 # 2. Add "pytest" as integration testing script as part of Github Actions CI workflow (you've done this before!)
